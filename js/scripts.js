541f9186ca7d706ead1128dd8ac79accb634b61f
rjashby <rjapsu160@gmail.com>
2021-10-22 12:59:12 -0700

    WIP - experimenting with name input field for results.

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     $('#javascript').hide();
     $('#python').hide();
 
+    const firstName = $("#name").val();
     const yourAge = parseInt($("input#age").val());
     const yourDOB = $("#born").val();
     const favColor = $("#color").val();
@@ -12,10 +13,14 @@ $(document).ready(function() {
     const favCereal = $("input:radio[name=cereal]:checked").val();
 
     if (favCereal === "Bacon" || (favPodcast === "Freakonomics")) {
+      $("#name").text(firstName);
       $('#csharp').fadeIn();
     } else if (favCereal === "Cheerios" && (yourAge <= 25)) {
+      $("#name").text(firstName);
       $('#javascript').fadeIn();
+     
     } else {
+      $("#name").text(firstName);
       $('#python').fadeIn();
     }
   });
